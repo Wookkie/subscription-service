8aabb06748d072006596281dda63cf35b87e9f29
Margarita Popova <nitritka93@gmail.com>
2026-04-25 16:10:52 +0300

    add config + server init

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,19 @@
 package config
+
+import "flag"
+
+type Config struct {
+	Host string
+	Port int
+}
+
+func ReadConfig() *Config {
+	var cfg Config
+
+	flag.StringVar(&cfg.Host, "host", "0.0.0.0", "flag for configure host")
+	flag.IntVar(&cfg.Port, "port", 8080, "flag for configure host")
+
+	flag.Parse()
+
+	return &cfg
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,51 @@
 package server
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	cfg       any
+	httpServe *http.Server
+}
+
+func New(host string, port int) *Server {
+	httpServe := http.Server{
+		Addr: host + ":" + strconv.Itoa(port),
+	}
+
+	myServer := Server{
+		httpServe: &httpServe,
+	}
+
+	myServer.configRoutes()
+
+	return &myServer
+}
+
+func (s *Server) configRoutes() {
+	router := gin.Default()
+	router.GET("/")
+	subscriptions := router.Group("/subscriptions")
+
+	subscriptions.GET("/")
+	subscriptions.GET("/:id")
+	subscriptions.POST("/")
+	subscriptions.PUT("/:id")
+	subscriptions.DELETE("/:id")
+
+	s.httpServe.Handler = router
+
+}
+
+func (s *Server) Run() error {
+	return s.httpServe.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServe.Shutdown(ctx)
+}
